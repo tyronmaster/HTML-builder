f0d07393c4fde62d38b70e697af97a3339d9050f
tyronmaster <degonski.art@gmail.com>
2022-05-14 09:59:53 +0200

    feat: add realisation 01 with ReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,30 @@
 const fs = require('fs');
 const path = require('path');
+const fileName = 'text.txt';
 
+// Simple variant
+/*
 fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
   if (err) console.log(err);
   console.log(data);
 });
+*/
+
+// Variant with Stream
+
+const myReadStream = fs.createReadStream(path.join(__dirname, fileName), 'utf-8');
+myReadStream.on('data', (chunk) => {
+  console.log(`Chunk or total file ${path.join(__dirname, fileName)} received :`);
+  console.log(chunk);
+});
+
+
+// TODO Variant with Stream & async
+/*
+const getFile = () => async (req, res) => {
+  const fileStream = await fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
+  res.contentType('text/html');
+  fileStream.pipe(res);
+};
+getFile();
+*/
